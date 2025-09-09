64eb77ec85d75df81bc7500a8af092e94c202b80
shadownocas <inessimoeschoco1@gmail.com>
2025-09-09 20:25:37 +0100

    makefiles and wtnot

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ines/robotica/intro_robotics/src
+# Build directory: /home/ines/robotica/intro_robotics/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ines/robotica/intro_robotics/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/ines/robotica/intro_robotics/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ines/robotica/intro_robotics/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ines/robotica/intro_robotics/src:$ROS_PACKAGE_PATH"
